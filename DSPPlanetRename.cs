3552f07ed5c250ccb935c21b1d2d7f89196503b0
GreyHak <479851+GreyHak@users.noreply.github.com>
2021-03-09 21:43:06 -0800

    Added in-game name-change mechanic

--- DSPPlanetRename.cs
+++ DSPPlanetRename.cs
@@ -7,8 +7,17 @@ using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
+using System.Security;
 using System.Security.Cryptography;
+using System.Security.Permissions;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
 namespace DSPPlanetRename
 {
     [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
@@ -21,6 +30,14 @@ namespace DSPPlanetRename
         new internal static ManualLogSource Logger;
         new internal static BepInEx.Configuration.ConfigFile Config;
         Harmony harmony;
+
+        public class PlanetNamingData
+        {
+            public int planetId;
+            public string originalName;
+            public string customName;
+        }
+        public static Dictionary<int, PlanetNamingData> nameDictionary = new Dictionary<int, PlanetNamingData>();
 
         public void Awake()
         {
@@ -36,10 +53,10 @@ namespace DSPPlanetRename
             return galaxy.seed.ToString("D8") + "-" + galaxy.starCount.ToString();
         }
 
-        public static string PlanetHash(PlanetData planet, GalaxyData galaxy)
+        public static string PlanetHash(string originalPlanetName, GalaxyData galaxy)
         {
             HashAlgorithm algorithm = SHA256.Create();
-            byte[] bytes = algorithm.ComputeHash(Encoding.UTF8.GetBytes(GetGalaxyName(galaxy) + planet.name));
+            byte[] bytes = algorithm.ComputeHash(Encoding.UTF8.GetBytes(GetGalaxyName(galaxy) + originalPlanetName));
 
             StringBuilder sb = new StringBuilder();
             foreach (byte b in bytes)
@@ -54,7 +71,93 @@ namespace DSPPlanetRename
             if (DSPGame.IsMenuDemo)
                 return;
 
-            __result.name = Config.Bind<string>("Planet Names", PlanetHash(__result, galaxy), __result.name, "[Cluster " + GetGalaxyName(galaxy) + "] Replacement planet name for " + __result.name).Value;
+            string customName = Config.Bind<string>("Planet Names", PlanetHash(__result.name, galaxy), __result.name, "[Cluster " + GetGalaxyName(galaxy) + "] Replacement planet name for " + __result.name).Value;
+            if (customName != __result.name)
+                Logger.LogDebug("Loading planet " + __result.name + "'s name " + customName);
+            nameDictionary.Add(__result.id, new PlanetNamingData
+            {
+                planetId = __result.id,
+                originalName = __result.name,
+                customName = customName
+            });
+            __result.name = customName;
+        }
+
+        public static DefaultControls.Resources uiResources;
+        public static GameObject nameInputObject = null;
+        public static InputField nameInput = null;
+        public static PlanetData activePlanet = null;
+
+        [HarmonyPostfix, HarmonyPatch(typeof(UIPlanetDetail), "_OnCreate")]
+        public static void UIPlanetDetail_OnCreate_Postfix(UIPlanetDetail __instance)
+        {
+            if (nameInputObject == null)
+            {
+                uiResources = new DefaultControls.Resources();
+                nameInputObject = DefaultControls.CreateInputField(uiResources);
+                nameInput = nameInputObject.GetComponent<InputField>();
+
+                nameInputObject.transform.SetParent(__instance.transform.parent);
+                nameInputObject.transform.localPosition = new Vector3(-145f, -38f, 0f);  // Moving the shorter one
+                nameInputObject.transform.localScale = new Vector3(1.06f, 1.3f, 1f);
+                nameInput.image.color = new Color(0, 0, 0, 0);
+                nameInput.textComponent.color = new Color(1, 1, 1, 1);
+
+                nameInput.onValueChanged.AddListener(new UnityAction<string>(OnNameInputSubmit));
+                nameInput.onEndEdit.AddListener(new UnityAction<string>(OnNameInputSubmit));
+
+                nameInputObject.SetActive(false);
+
+                __instance.nameText.enabled = false;
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(UIPlanetDetail), "_OnOpen")]
+        public static void UIPlanetDetail_OnOpen_Postfix(UIPlanetDetail __instance)
+        {
+            nameInputObject.SetActive(true);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(UIPlanetDetail), "_OnClose")]
+        public static void UIPlanetDetail_OnClose_Postfix(UIPlanetDetail __instance)
+        {
+            activePlanet = null;
+            nameInputObject.SetActive(false);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(UIPlanetDetail), "OnPlanetDataSet")]
+        public static void UIPlanetDetail_OnPlanetDataSet_Postfix(UIPlanetDetail __instance)
+        {
+            if (__instance.planet != null && ((activePlanet == null) || (__instance.planet.id != activePlanet.id)))
+            {
+                activePlanet = __instance.planet;
+                nameInput.text = __instance.planet.name;
+            }
+        }
+
+        public static void OnNameInputSubmit(string s)
+        {
+            if (activePlanet != null)
+            {
+                string newPlanetName = nameInput.text.Trim();
+
+                PlanetNamingData planetNamingData = nameDictionary[activePlanet.id];
+
+                if (newPlanetName.Length == 0)
+                {
+                    newPlanetName = planetNamingData.originalName;
+                }
+
+                Logger.LogDebug("Changing planet " + planetNamingData.originalName + "'s name from " + planetNamingData.customName + " to " + newPlanetName);
+                Config.Bind<string>("Planet Names", PlanetHash(planetNamingData.originalName, GameMain.data.galaxy), planetNamingData.originalName, "[Cluster " + GetGalaxyName(GameMain.data.galaxy) + "] Replacement planet name for " + planetNamingData.originalName).Value =
+                    planetNamingData.customName = activePlanet.name = newPlanetName;
+
+                UIRoot.instance.uiGame.planetDetail.OnPlanetDataSet();
+                foreach (UIStarmapPlanet uistarmapPlanet in UIRoot.instance.uiGame.starmap.planetUIs)
+                {
+                    uistarmapPlanet.nameText.text = uistarmapPlanet.planet.name;
+                }
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -2,21 +2,23 @@
 
 **DSP Planet Rename** is a mod for the Unity game Dyson Sphere Program developed by Youthcat Studio and published by Gamera Game.  The game is available on [here](https://store.steampowered.com/app/1366540/Dyson_Sphere_Program/).
 
-With this mod you can rename planets.
+With this mod you can rename planets.  Planets are renamed from the star map view by clicking on the planet name above the planet details on the right.
 
 If you like this mod, please click the thumbs up at the [top of the page](https://dsp.thunderstore.io/package/GreyHak/DSP_Planet_Rename/) (next to the Total rating).  That would be a nice thank you for me, and help other people to find a mod you enjoy.
 
 If you have issues with this mod, please report them on [GitHub](https://github.com/GreyHak/dsp-planet-rename/issues).  I try to respond within 12 hours.    You can also contact me at GreyHak#2995 on the [DSP Modding](https://discord.gg/XxhyTNte) Discord #tech-support channel..
 
+If you want to rename stars, please see [kassent](https://dsp.thunderstore.io/package/kassent/)'s [StarmapExtension](https://dsp.thunderstore.io/package/kassent/StarmapExtension/) mod.
+
 ## Config File
+The config file is used to store the planet names.  This will ensure you are able to disable this mod and without having issues with your save file.
+
 The configuration file is called `greyhak.dysonsphereprogram.planetrename.cfg`.  It is generated the first time you run the game with this mod installed.  On Windows 10 it is located at
  - If you installed manually:  `%PROGRAMFILES(X86)%\Steam\steamapps\common\Dyson Sphere Program\BepInEx\config\greyhak.dysonsphereprogram.planetrename.cfg`
  - If you installed with r2modman:  `C:\Users\<username>\AppData\Roaming\r2modmanPlus-local\DysonSphereProgram\profiles\Default\BepInEx\config\greyhak.dysonsphereprogram.planetrename.cfg`
 
-The config file is used to store the planet names.  This will ensure you are able to disable this mod and now have issues with your save file.
-
 ## Installation
-This mod uses the BepInEx mod plugin framework.  So BepInEx must be installed to use this mod.  Find details for installing BepInEx [in their user guide](https://bepinex.github.io/bepinex_docs/master/articles/user_guide/installation/index.html#installing-bepinex-1).  This mod was tested with BepInEx x64 5.4.5.0 and Dyson Sphere Program 0.6.16.5780 on Windows 10.
+This mod uses the BepInEx mod plugin framework.  So BepInEx must be installed to use this mod.  Find details for installing BepInEx [in their user guide](https://bepinex.github.io/bepinex_docs/master/articles/user_guide/installation/index.html#installing-bepinex-1).  This mod was tested with BepInEx x64 5.4.5.0 and Dyson Sphere Program 0.6.16.5831 on Windows 10.
 
 To manually install this mod, add the `DSPPlanetRename.dll` to your `%PROGRAMFILES(X86)%\Steam\steamapps\common\Dyson Sphere Program\BepInEx\plugins\` folder.
 
